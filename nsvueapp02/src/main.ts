1749d1988b2a8b92d564487fbea0e0a619ac3c70
gel1123 <atno.14@gmail.com>
2021-11-14 22:36:12 +0900

    add font and icon

--- nsvueapp02/src/main.ts
+++ nsvueapp02/src/main.ts
@@ -1,9 +1,20 @@
 import Vue from 'nativescript-vue'
 import App from './components/App.vue'
 import VueDevtools from 'nativescript-vue-devtools'
+import FontIcon from 'nativescript-vue-fonticon'
+import './app.scss'
+
+Vue.use(FontIcon, {
+  componentName: 'FIcon',
+  debug: true,
+  paths: {
+    mdi: './fonts/material-design-icons.css',
+  }
+})
+
 
 console.log(`---------------------- [TNS_ENV: ${TNS_ENV} ] ----------------------`);
-if(TNS_ENV !== 'production') {
+if (TNS_ENV !== 'production') {
   Vue.use(VueDevtools, { host: '10.0.3.2' })
 }
 
@@ -13,6 +24,6 @@ Vue.config.silent = (TNS_ENV === 'production')
 
 
 new Vue({
-  
+
   render: h => h('frame', [h(App)])
 }).$start()
