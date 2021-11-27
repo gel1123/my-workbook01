d2c4d5f2873a1afc304be76dd556955e63fb91d8
gel1123 <atno.14@gmail.com>
2021-11-27 12:07:48 +0900

    プロキシ統合なにもわからない

--- cdkapp01/lambda/lambdaHandler.ts
+++ cdkapp01/lambda/lambdaHandler.ts
@@ -1,6 +1,7 @@
 import * as sdk from 'aws-sdk';
 import { PutItemInput, PutItemInputAttributeMap } from 'aws-sdk/clients/dynamodb';
 import { PromiseResult } from 'aws-sdk/lib/request';
+import { callbackify } from 'util';
 
 const Region = process.env.REGION!;
 const TABLE_NAME = process.env.TABLE_NAME!;
@@ -12,17 +13,79 @@ export interface Memo {
     memo_id: string
 }
 
+/**
+ * メモを読むLambda
+ * @param memo 
+ * @returns 
+ * 
+ * ### 補足
+ * 未実装だが参考にすべきリンク：
+ *   [DynamoDB でのエラー処理](https://docs.aws.amazon.com/ja_jp/amazondynamodb/latest/developerguide/Programming.Errors.html)
+ */
 export async function memoHandler(memo: Memo)
     : Promise<PromiseResult<sdk.DynamoDB.GetItemOutput, sdk.AWSError>> {
 
+    // 最後に実行している aws-sdk >> Request::promiseメソッドは、
+    // 旧来の コールバック地獄 / Promiseラップ地獄 を回避するためにAWS-SDKが用意してくれたもの。
+    // 応答をPromiseで返却してくれる。
+    // 参考：https://qiita.com/taqm/items/f22ee3c3f0fb3d433a85
     return Dynamo.getItem({
         TableName: TABLE_NAME,
         Key: { memo_id: { S: memo.memo_id } }
     }).promise();
 }
 
-export async function writeHandler(arg: any)
-    : Promise<PromiseResult<sdk.DynamoDB.PutItemOutput, sdk.AWSError>> {
+/**
+ * プロキシ統合におけるLambdaの要求形式の一部.
+ * bodyを持っていることを明示的にしたいがために、このような定義としている。
+ * 
+ * ※body以外にも色々とキーがあるはずだが、
+ *   ひとつひとつ書いていくのがとても大変なので、
+ *   すべてのインデックスシグネチャにanyを割り当てている。
+ * 
+ *   そうすることで、次の条件のオブジェクトを定義している
+ * 
+ *     * bodyは必須
+ *     * ほかはなんでもよい
+ * 
+ *   ※もっと良いやり方あればだれか教えてほしいです...!
+ * 
+ *   ※当初 `[key: string | number]: any;`と書いていたら、コンパイラに次のように怒られた。
+ *   
+ *   ```
+ *   lambda/lambdaHandler.ts:55:6 - error TS1337: An index signature parameter type cannot be a union type. Consider using a mapped object type instead.
+ *   ```
+ */
+export interface proxyReq {
+    body: string;
+    [key: string]: any;
+}
+
+/**
+ * プロキシ統合におけるLambdaが沿うべき応答形式
+ */
+export interface proxyRes {
+    statusCode: number;
+    body: string;
+    isBase64Encoded?: true | false;
+    headers?: object;
+    multiValueHeaders?: object;
+}
+
+/**
+ * メモを書くLambda。
+ * プロキシ統合でAPI Gatewayに組み込むので、
+ * 応答を特定の形式にする必要がある。
+ * 
+ * また、プロキシ統合で組込むためasync関数ではなく、通常の関数として定義する...つもりだが、
+ * 色々確認してからにする。
+ * 
+ * @param req 
+ * @returns 
+ */
+export function writeHandler(req: proxyReq)
+    : proxyRes {
+
 
     const now = new Date();
     const d = {
@@ -38,11 +101,34 @@ export async function writeHandler(arg: any)
 
     const newItem: PutItemInputAttributeMap = {
         memo_id: { S: "101" },
-        body: { S: `このメモは ${datestr} に作成されました。引数には次の値が指定されています：${JSON.stringify(arg)}` }
+        body: { S: `このメモは ${datestr} に作成されました。引数には次の値が指定されています：${JSON.stringify(req)}` }
     }
     const input: PutItemInput = {
         TableName: TABLE_NAME,
         Item: newItem
     }
-    return Dynamo.putItem(input).promise();
+    // ---- ※補足 ----
+    // asyncではない関数内で、いきなり下記のようにawaitを使うのはNG!
+    //
+    // ```
+    // const result = await Dynamo.putItem(input).promise();
+    // ```
+    //
+    // これは awaitがさながら Step Functions のように動作するからである。
+    // awaitはいっけん「同期的に動く」ように見えるが、
+    // その実、「非同期で動く」ものである。
+    // そのためasyncではない関数で使うことは、できない。
+    //
+    // 非同期だが、ステップで連動する関数を生成することで、
+    // いっけん同期的に動いているように見せかけるもの。
+    //
+    // 参考：https://qiita.com/jun1s/items/ecf6965398e00b246249
+    //
+
+    const result: sdk.Request<sdk.DynamoDB.PutItemOutput, sdk.AWSError> = Dynamo.putItem(input);
+    const res: proxyRes = {
+        statusCode: 200,
+        body: `${result.startTime.getMinutes()}分：実行開始`
+    }
+    return res;
 }
