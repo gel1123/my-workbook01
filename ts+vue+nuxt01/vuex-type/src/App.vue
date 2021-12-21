c5a6e1dbb9153fe30f55fee9f82a51d3e41ad08b
gel1123 <atno.14@gmail.com>
2021-12-21 09:22:46 +0900

    vuexでの型定義の問題について触れてみる

---
+++ ts+vue+nuxt01/vuex-type/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" @click="setName">
+  <p>state[name] is "{{$store.state.name}}"</p>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    setName() {
+      const s = (this as any).$store;
+      s.dispatch('asyncSetName', "sig")
+    }
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
