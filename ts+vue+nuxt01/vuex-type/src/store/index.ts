c521ea4efec2908896086609169e96ed1f1d9916
gel1123 <atno.14@gmail.com>
2021-12-26 15:47:27 +0900

    Vuexのgettersで型推論を効かせる

--- ts+vue+nuxt01/vuex-type/src/store/index.ts
+++ ts+vue+nuxt01/vuex-type/src/store/index.ts
@@ -1,26 +1,46 @@
 import { createStore } from 'vuex'
 
+interface State {
+  count: number | null;
+  name: string | null;
+}
+const state: State = {
+  count: 0,
+  name: null
+}
+/** Storeの「getters」が内包するメソッドのキーと返り値の型 */
+interface IGetters {
+  getName: string | null;
+  greet: string;
+}
+/** Storeの「getters」全体としての型定義。これでゲッターが別のゲッターを参照する際の静的検査が有効になる */
+type Getters<S, G> = {
+  [K in keyof G]: (state: S, getters: G) => G[K];
+};
+const getters:Getters<State, IGetters> = {
+  getName(state: State, getters) {
+    return state.name
+  },
+  greet(state: State, getters) {
+    //type Getters<State, IGetters> により、変数gettersが型定義され、
+    // 下記のエラーを事前に検出できるようになる。
+    // （なにも対策を練らないと、gettersはany型となってしまうため、このような自前の型定義が必要）
+    // return `My name is ${getters.getName.toUpperCase()}`
+
+    // コンパイルエラーが出ないよう書くなら、こう
+    return `My name is ${getters.getName?.toUpperCase()}`;
+  }
+}
+
 export default createStore({
-  state: {
-    count: 0 as number | null,
-    name: null as string | null,
-  },
-  getters: {
-    getName(state, getters) {
-      return state.name
-    },
-    /** コンパイルエラーは出ないが、実際に実行すると「Error: Cannot read properties of null」 */
-    greet(state, getters) {
-      // gettersがany型であるために、getNameもany型になる。その結果、nullの可能性を検出できない
-      return `My name is ${getters.getName.toUpperCase()}`
-    }
-  },
+  state: state,
+  getters: getters,
   mutations: {
-    setName(state, payload) {
+    setName(state: State, payload) {
       // state.name = payload; // payloadもany。なのでスキーマ誤りを検出できない
       state.name = payload.name;
     },
-    increment(state, payload) {
+    increment(state: State, payload) {
       // state.count++; // これはコンパイルエラー（nullかもしれないから）
     }
   },
