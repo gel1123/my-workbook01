7851d4c9caa4d9a25557cacec72f32849d750f59
gel1123 <atno.14@gmail.com>
2021-12-29 14:46:18 +0900

    Vuexのmutationsとactionsで型推論を効かせる

--- ts+vue+nuxt01/vuex-type/src/store/index.ts
+++ ts+vue+nuxt01/vuex-type/src/store/index.ts
@@ -31,39 +31,77 @@ const getters:Getters<State, IGetters> = {
     return `My name is ${getters.getName?.toUpperCase()}`;
   }
 }
+/**
+ * Mustationsの各メソッドで指定できるのは、
+ * メソッド名と第二引数「payload」の型だけ。
+ * これに従い、IMutationsでは、関数名とpayloadの型を定義する。
+ * payloadなしなら、voidとする。
+ */
+interface IMutations {
+  setName: {name: string};
+  increment: void;
+}
+/** Mutations全体の型 */
+type Mutations<S, M> = {
+  [K in keyof M]: (state: S, payload: M[K]) => void;
+}
+const mutations: Mutations<State, IMutations> = {
+  setName(state: State, payload) {
+    // 独自定義した型注釈のおかげで型推論が有効になっている
+    state.name = payload.name;
+  },
+  increment(state: State) {
+    // 独自定義した型注釈のおかげで型推論が有効になっている
+    state.count = state.count ? state.count + 1 : 1
+  }
+};
+/**
+ * Actionsの各メソッドも、Mutationsと同じく自由になるのは
+ * 関数名と第二引数「payload」。
+ */
+interface IActions {
+  asyncSetName: string;
+  asyncIncrement: void;
+  countup: void
+}
+/** CommitはMutationsのメソッド名を文字列で指定して実行する */
+type Commit<M> = <T extends keyof M>(type: T, payload?: M[T]) => void;
+/** DispatchはActionsのメソッド名を文字列で指定して実行する */
+type Dispatch<A> = <T extends keyof A>(type: T, payload?: A[T]) => any;
+type Context<S, A, G, M> = {
+  commit: Commit<M>
+  dispatch: Dispatch<A>
+  state: S,
+  getters: G
+}
+type Actions<S, A, G = {}, M = {}> = {
+  [K in keyof A]: (ctx: Context<S, A, G, M>, payload: A[K]) => any;
+}
+const actions: Actions<State, IActions, IGetters, IMutations> = {
+  asyncSetName(ctx, payload) {
+    ctx.commit('setName', {name: payload});
+    console.log(ctx.state.name);
+  },
+  asyncIncrement(ctx) {
+    ctx.commit('increment');
+    console.log(ctx.state.count);
+  },
+  async countup(ctx) {
+    await (() => new Promise(resolve => {
+      setTimeout(resolve, 1000);
+    }))();
 
+    // dispatchはActionの実行。ここでしたいのはMutationの実行なのでcommitが正しい
+    // だが、こう書いても実行するまでエラーが出ない
+    // ctx.dispatch('increment');
+    ctx.commit('increment');
+  }
+}
 export default createStore({
   state: state,
   getters: getters,
-  mutations: {
-    setName(state: State, payload) {
-      // state.name = payload; // payloadもany。なのでスキーマ誤りを検出できない
-      state.name = payload.name;
-    },
-    increment(state: State, payload) {
-      // state.count++; // これはコンパイルエラー（nullかもしれないから）
-    }
-  },
-  actions: {
-    asyncSetName(ctx, payload) {
-      ctx.commit('setName', {name: payload});
-      console.log(ctx.state.name);
-    },
-    asyncIncrement(ctx) {
-      ctx.commit('increment');
-      console.log(ctx.state.count);
-    },
-    async countup(ctx) {
-      await (() => new Promise(resolve => {
-        setTimeout(resolve, 1000);
-      }))();
-
-      // dispatchはActionの実行。ここでしたいのはMutationの実行なのでcommitが正しい
-      // だが、こう書いても実行するまでエラーが出ない
-      // ctx.dispatch('increment');
-      ctx.commit('increment');
-    }
-  },
+  mutations: mutations,
+  actions: actions,
   modules: {
   }
 })
