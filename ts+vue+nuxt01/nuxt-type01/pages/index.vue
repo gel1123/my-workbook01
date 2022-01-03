3718859b49e1551274d60ad2b6783abda6f0322b
gel1123 <atno.14@gmail.com>
2022-01-03 11:50:05 +0900

    下準備

--- ts+vue+nuxt01/nuxt-type01/pages/index.vue
+++ ts+vue+nuxt01/nuxt-type01/pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <Tutorial/>
+  <section>
+    <NuxtLogo/>
+    <h1 class="title">{{title}}</h1>
+    <h2 class="subtitle">{{subtitle}}</h2>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
+interface IData {
+  title: string
+}
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data(): IData {
+    return {
+      title: 'Nuxt * TypeScript'
+    }
+  },
+  computed: {
+    subtitle(): string {
+      return `"${this.title}" は Nuxt.js + TypeScript の型定義について学習するためのプロジェクトです`
+    }
+  }
 })
 </script>
