aa9ccf97de325741485d62a18139435a12c645f9
gel1123 <atno.14@gmail.com>
2022-01-08 23:41:38 +0900

    jumbotron * 2

--- sugar/sugar-nuxtapp01/components/jumbotron.vue
+++ sugar/sugar-nuxtapp01/components/jumbotron.vue
@@ -1,6 +1,22 @@
+<script lang="ts" setup>
+const src = ref("/img/jumbotron01_700-1100.jpeg");
+const switchSrc = () => {
+    const srcList = [
+        "/img/jumbotron01_700-1100.jpeg",
+        "/img/jumbotron02_700-1100.jpeg"
+    ];
+    const beforeIndex = srcList.indexOf(src.value)
+    if (srcList.length-1 === beforeIndex) {
+        src.value = srcList[0];
+    } else {
+        src.value = srcList[beforeIndex+1];
+    }
+}
+</script>
 <template>
     <div class="jumbotron">
-        <img src="/img/jumbotron01_700-1100.jpeg">
+        <img :src="src" 
+            @animationiteration="switchSrc">
     </div>
 </template>
 <style scoped>
@@ -14,10 +30,13 @@ img {
     width: 100%;
     object-fit: cover;
     object-position: 0% top;
-    animation: 10s ease-in 1s 1 forwards slide;
+    animation: 20s ease-in 1s infinite forwards slide;
+    opacity: 0;
 }
 @keyframes slide {
-    0% { object-position: 0% top; }
-    100% { object-position: 100% top; }
+      0% { opacity: 0; object-position:   0% top; }
+     10% { opacity: 1; object-position:   0% top; }
+     90% { opacity: 1; object-position:  90% top; }
+    100% { opacity: 0; object-position: 100% top; }
 }
 </style>
