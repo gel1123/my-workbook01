a238477f41448a7f12b6815ca248afab8387f918
gel1123 <atno.14@gmail.com>
2022-01-10 08:14:20 +0900

    レスポンシブ化

--- sugar/sugar-nuxtapp01/pages/index.vue
+++ sugar/sugar-nuxtapp01/pages/index.vue
@@ -103,90 +103,94 @@ onMounted(async () => {
   <Title>{{title}}</Title>
   <Body>
     <Modal1 v-if="modalSrc" :src="modalSrc" @close-picture="closePicture"/>
-    <div class="wrapper">
-      <div class="top">
-        <Logo/>
-        <div class="top__description">
-          <p>
-            Drip CafeのCOFFEEは一杯一杯ドリップしてお出ししているので
-            いつでも新鮮なコーヒーをお楽しみいただけます。
-          </p>
-          <p>
-            他にも沖縄で育った琉球紅茶シリーズ、
-            夏にはかかせないスムージードリンクなどもございます。
-          </p>
-          <p>
-            人気のビスケットとご一緒にCOFFEEタイムをお楽しみ下さい。
-          </p>
+    <div class="wrapper_wrapper">
+      <div class="wrapper">
+        <div class="top">
+          <Logo/>
+          <div class="top__description">
+            <p>
+              Drip CafeのCOFFEEは一杯一杯ドリップしてお出ししているので
+              いつでも新鮮なコーヒーをお楽しみいただけます。
+            </p>
+            <p>
+              他にも沖縄で育った琉球紅茶シリーズ、
+              夏にはかかせないスムージードリンクなどもございます。
+            </p>
+            <p>
+              人気のビスケットとご一緒にCOFFEEタイムをお楽しみ下さい。
+            </p>
+          </div>
+          <Jumbotron1 />
         </div>
-        <Jumbotron1 />
-      </div>
 
-      <!-- <div class="story body">
-        <h2 class="story__heading">{{title}}のこだわり</h2>
-        <div class="story__body text--minimum">
-          ストーリー、こだわり、価値観などブランドイメージを伝える
-        </div>
-      </div> -->
+        <!-- <div class="story body">
+          <h2 class="story__heading">{{title}}のこだわり</h2>
+          <div class="story__body text--minimum">
+            ストーリー、こだわり、価値観などブランドイメージを伝える
+          </div>
+        </div> -->
 
-      <div class="about body">
-        <h2 class="about__heading">{{title}}について</h2>
-        <div class="about__shop-info">
-          <h3 class="about__shop-info__heading">店舗情報</h3>
-          <table>
-            <tr>
-              <td>営業時間</td>
-              <td>：12時00分～18時30分</td>
-            </tr>
-            <tr>
-              <td>定休日</td>
-              <td>：水曜日</td>
-            </tr>
-          </table>
-          <div class="about__address">
-            <h4 class="about__address__heading">住所</h4>
-            <p>〒901-0225 沖縄県豊見城市豊崎１−４１１<br>
-            豊崎ライフスタイルセンター TOMITON 1階</p>
-            <div class="about__address__map">
-              <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57291.284168778!2d127.66559968824788!3d26.173726798606367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f1!3m3!1m2!1s0x34e5680b5774aceb%3A0x69e998bd1e2abe73!2sDRIP%20CAFE!5e0!3m2!1sja!2sjp!4v1641312151258!5m2!1sja!2sjp"
-                width="90%" height="200px" style="border:0;" loading="lazy"></iframe>
+        <div class="about body">
+          <h2 class="about__heading">{{title}}について</h2>
+          <div class="about__shop-info">
+            <h3 class="about__shop-info__heading">店舗情報</h3>
+            <table>
+              <tr>
+                <td>営業時間</td>
+                <td>：12時00分～18時30分</td>
+              </tr>
+              <tr>
+                <td>定休日</td>
+                <td>：水曜日</td>
+              </tr>
+            </table>
+            <div class="about__address">
+              <h4 class="about__address__heading">住所</h4>
+              <p>〒901-0225 沖縄県豊見城市豊崎１−４１１<br>
+              豊崎ライフスタイルセンター TOMITON 1階</p>
+              <div class="about__address__map">
+                <iframe 
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57291.284168778!2d127.66559968824788!3d26.173726798606367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f1!3m3!1m2!1s0x34e5680b5774aceb%3A0x69e998bd1e2abe73!2sDRIP%20CAFE!5e0!3m2!1sja!2sjp!4v1641312151258!5m2!1sja!2sjp"
+                  width="90%" height="200px" style="border:0;" loading="lazy"></iframe>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="gallery body">
-        <h2 class="gallery__heading">Gallery</h2>
-        <div class="gallery__body">
-          <Gallery @show-picture="showPicture" />
+        <div class="gallery body">
+          <h2 class="gallery__heading">Gallery</h2>
+          <div class="gallery__body">
+            <Gallery @show-picture="showPicture" />
+          </div>
         </div>
-      </div>
-      <div class="blog body bottom">
-        <h2 class="blog__heading">{{title}}'s Blog</h2>
-        <div class="blog__body">
-          <div v-for="item in rssItems" :v-key="item.link">
-            <div style="display: flex;">
-              <div v-if="item.enclosure.url" style="width: 50%; margin: 8px 8px; align-items: center;">
-                <p style="font-size: small; margin: 0 0 6px 0;">{{item.title}} ({{item.pubDate}})</p>
-                <p style="font-size: small;">{{item.summary}}<a :href="item.link" target="_blank" rel="noopener noreferrer">続き</a></p>
-              </div>
-              <div v-if="!item.enclosure.url" style="width: 100%; margin: 8px 8px; align-items: center;">
-                <p style="font-size: small; margin: 0 0 6px 0;">{{item.title}} ({{item.pubDate}})</p>
-                <p style="font-size: small;">{{item.summary}}<a :href="item.link" target="_blank" rel="noopener noreferrer">続き</a></p>
-              </div>
-              <div v-if="item.enclosure.url" style="width: 50%; margin: auto 8px; align-items: center;">
-                <img :src="item.enclosure.url" 
-                  style="height: 80%; width: 80%; object-fit: cover; object-position: 50% 50%;"/>
+        <div class="blog body bottom">
+          <h2 class="blog__heading">{{title}}'s Blog</h2>
+          <div class="blog__body">
+            <div class="blog__item_wrapper">
+              <div class="blog__item" v-for="item in rssItems" :v-key="item.link">
+                <div class="blog__item_child" style="display: flex;">
+                  <div class="blog__item__text_wrapper" v-if="item.enclosure.url" style="width: 50%; margin: 8px 8px; align-items: center;">
+                    <p style="font-size: small; margin: 0 0 6px 0;">{{item.title}} ({{item.pubDate}})</p>
+                    <p style="font-size: small;">{{item.summary}}<a :href="item.link" target="_blank" rel="noopener noreferrer">続き</a></p>
+                  </div>
+                  <div class="blog__item__text_wrapper" v-if="!item.enclosure.url" style="width: 100%; margin: 8px 8px; align-items: center;">
+                    <p style="font-size: small; margin: 0 0 6px 0;">{{item.title}} ({{item.pubDate}})</p>
+                    <p style="font-size: small;">{{item.summary}}<a :href="item.link" target="_blank" rel="noopener noreferrer">続き</a></p>
+                  </div>
+                  <div class="blog__item__img_wrapper" v-if="item.enclosure.url" style="width: 50%; margin: auto 8px; align-items: center;">
+                    <img :src="item.enclosure.url" 
+                      style="height: 80%; width: 80%; object-fit: cover; object-position: 50% 50%;"/>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <footer>
+      © 2022 Drip Cafe
+    </footer>
   </Body>
-  <footer>
-    © 2022 Drip Cafe
-  </footer>
 </template>
 <style scoped>
 .wrapper {
@@ -247,5 +251,35 @@ footer {
   font-size: smaller;
   color: rgb(100, 100, 100);;
   background-color: rgb(240, 240, 240);
+}
+
+@media screen and (min-width: 450px) {
+  .wrapper {
+    max-width: 600px;
+    margin: auto;
+  }
+  .wrapper_wrapper {
+    margin: auto;
+    max-width: 650px;
+    padding: 0 20px;
+    background-color: white;
+  }
+  footer {
+    max-width: 690px;
+    margin: 0 auto;
+  }
+  .blog__item {
+    width: 30%;
+  }
+  .blog__item_wrapper {
+    display: flex;
+    justify-content: center;
+  }
+  .blog__item__text_wrapper, .blog__item__img_wrapper {
+    width: 90%!important;
+  }
+  .blog__item_child {
+    display: block!important;
+  }
 }
 </style>
