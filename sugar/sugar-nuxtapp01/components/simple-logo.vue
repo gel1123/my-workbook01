fd83b66de7377cf1d6103ea94d72bab3b9d10534
gel1123 <atno.14@gmail.com>
2022-01-10 06:32:00 +0900

    fix header

--- sugar/sugar-nuxtapp01/components/simple-logo.vue
+++ sugar/sugar-nuxtapp01/components/simple-logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo">
-    <div class="cover c01"></div>
+    <img src="/img/simple-logo_110-265.png" />
   </div>
 </template>
 <style scoped>
@@ -8,16 +8,8 @@
   width: 100%;
   position: absolute;
 }
-.c01 { left:  0%; background-color: rgba(230, 255, 255, 0.2); height: 180px;}
-.logo {
+img {
   width: 100%;
-  height: 180px;
-  background-size: cover;
-  /* border: 1px solid rgb(200, 200, 200); */
-  background-image: url(/img/simple-logo_180-260.png);
-  background-color: rgba(255, 255, 255, 0);
-  background-blend-mode:lighten;
-  background-repeat: no-repeat;
-  background-position: center;
+  object-fit: cover;
 }
 </style>
