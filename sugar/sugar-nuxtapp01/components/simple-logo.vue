9039866a6df9a0f689ea5402a6deb5e326f89545
gel1123 <atno.14@gmail.com>
2022-01-11 08:56:39 +0900

    ビルド時のリソース404対応

--- sugar/sugar-nuxtapp01/components/simple-logo.vue
+++ sugar/sugar-nuxtapp01/components/simple-logo.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="logo">
-    <img src="/img/simple-logo_110-265.png" />
+    <!-- v-bindなしで書くと、ビルド時に .output/public/_nuxt ディレクトリをコンテキストとみなして404になる -->
+    <!-- そのため、回避すべく v-bind:src="'文字列'" にしている. -->
+    <img :src="'/img/simple-logo_110-265.png'" />
   </div>
 </template>
 <style scoped>
@@ -13,3 +15,4 @@ img {
   object-fit: cover;
 }
 </style>
+
